4980bd18ab09c6510e14dfe91b9caaa3f40cd10a
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-02-24 19:25:21 +0530

    updated reducer

--- src/reducer.js
+++ src/reducer.js
@@ -66,6 +66,7 @@ const reducer = (state, action) => {
 
     case SET_LOADING:
       return { ...state, isLoading: true };
+
     case SET_STORIES:
       return {
         ...state,
@@ -73,13 +74,16 @@ const reducer = (state, action) => {
         hits: action.payload.hits,
         nbPages: action.payload.nbPages,
       };
+
     case REMOVE_STORY:
       return {
         ...state,
         hits: state.hits.filter((story) => story.objectID !== action.payload),
       };
+
     case HANDLE_SEARCH:
       return { ...state, query: action.payload, page: 0 };
+
     case HANDLE_PAGE:
       if (action.payload === "inc") {
         let nextPage = state.page + 1;
